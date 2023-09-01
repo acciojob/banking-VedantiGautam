42d0953f27bf5c62661f1b2d1ca8a8dcfbf8c7bc
VedantiGautam <gautamvedanti653@gmail.com>
2023-09-01 18:59:09 +0530

    bank

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -1,11 +1,14 @@
 package com.driver;
 
 public class SavingsAccount extends BankAccount{
-    double rate;
-    double maxWithdrawalLimit;
+    private double rate;
+    private double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
+        super(name, balance, 0);
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate = rate;
 
     }
     public void withdraw(double amount) throws Exception {
@@ -14,14 +17,47 @@ public class SavingsAccount extends BankAccount{
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
     }
+    public SavingsAccount() {
+    }
+
+    //--------------- Getter and Setter ----------------
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) throws Exception {
+        // Might throw the following errors:
+        // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
+        // 2. "Insufficient Balance" : If the amount exceeds balance
+        if(amount>maxWithdrawalLimit){
+            throw new Exception("Maximum Withdraw Limit Exceeded");
+        }
+        super.withdraw(amount);
+    }
+
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
+        return (this.getBalance()*years*this.rate/100)+this.getBalance();
 
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
+        return this.getBalance()*Math.pow((1+this.rate/(times*100)),times*years);
 
     }
 
